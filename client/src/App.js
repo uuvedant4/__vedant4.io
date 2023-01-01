9f10fa2c8b298643970ffc8bb86e29526ff1a2a7
vedant <ursvedantyetekar@gmail.com>
2023-01-01 21:17:25 +0530

    created header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;

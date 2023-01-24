720edae2eec4262608c954c60c3462bbdaedbee8
vedant <ursvedantyetekar@gmail.com>
2023-01-25 03:56:31 +0530

    created about page and footer component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import About from "./components/About/About.jsx";
 
 function App() {
   return (
@@ -9,7 +11,9 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,44 @@
+import { Typography } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+import { BsGithub, BsInstagram, BsLinkedin } from "react-icons/bs";
+import { SiLeetcode } from "react-icons/si";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div>
+        <Typography variant="h5">About Me</Typography>
+        <Typography>
+          Yo, it's <b>Vedant Yetekar</b>, a software developer who's coding cool
+          and crafting digital masterpieces.
+        </Typography>
+
+        <Link to="/contact" className="footerContactBtn">
+          <Typography>Contact Us</Typography>
+        </Link>
+      </div>
+      <div>
+        <Typography variant="h6">Social Media</Typography>
+        <a href="https://github.com/uuvedant4" target="black">
+          <BsGithub />
+        </a>
+        <a href="https://www.instagram.com/vedantyetekar7/" target="black">
+          <BsInstagram />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/vedant-yetekar-497949218"
+          target="black"
+        >
+          <BsLinkedin />
+        </a>
+        <a href="https://leetcode.com/__vedant4/" target="black">
+          <SiLeetcode />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;

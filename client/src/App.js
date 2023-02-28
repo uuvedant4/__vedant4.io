d0fecaabeda1caccaee0c4d222aa076b506200ab
vedant <ursvedantyetekar@gmail.com>
2023-03-01 04:05:30 +0530

    setup smtp

--- client/src/App.js
+++ client/src/App.js
@@ -24,6 +24,4 @@ function App() {
   );
 }
 
-// Vedant
-
 export default App;
--- server/routes/User.js
+++ server/routes/User.js
@@ -1,3 +1,19 @@
 import express from "express";
+import {
+  contact,
+  getUser,
+  login,
+  logout,
+  myProfile,
+} from "../controller/User.js";
+import { isAuthenticated } from "../middlewares/auth.js";
 
 export const userRouter = express.Router();
+
+userRouter.route("/login").post(login);
+userRouter.route("/logout").get(logout);
+userRouter.route("/user").get(getUser);
+userRouter.route("/me").get(isAuthenticated, myProfile);
+userRouter.route("/contact").post(contact);
+
+// 02:40
